ceb4e78a57bc1a1472cc010edb3655ac738d29c0
JRod166 <jose.vilca.vargas@gmail.com>
2018-04-17 11:24:02 -0500

    lineas de ejecucion

--- suma de vectores/vectores.c
+++ suma de vectores/vectores.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/// mpirun -np 3 ./vectores 11
 
 MPI_Status status;
 int suma=0;
@@ -21,19 +22,24 @@ int main(int argc, char *argv[]){
     int aux1=size-1;
     int resto=sizeVector%aux1;  
     vector=(int*)malloc(sizeVector*sizeof(int));
+    //generando vector
     if(rank==0){        
         for(int i=0;i<sizeVector;i++){
             vector[i]=i*1;      
         }       
-        for(int i=1;i<size;i++){            
+        //enviando vectores
+        for(int i=1;i<size;i++){
             MPI_Send(vector,sizeVector,MPI_INT,i,10,MPI_COMM_WORLD);                        
         }   
         int sumaTotal=0;
+        //sumando vectores
         for(int i=1;i<size;i++){
+            printf("Proceso: %d ",rank);
             MPI_Recv(&suma, 1000, MPI_INT, i, 10, MPI_COMM_WORLD,&status);
-            sumaTotal+=suma;
+            printf("%d + %d\n",sumaTotal,suma);
+            sumaTotal=sumaTotal+suma;
         }
-        printf("%d",sumaTotal);
+        printf("%d \n",sumaTotal);
 
     }   
     if(rank!=0){
@@ -41,13 +47,15 @@ int main(int argc, char *argv[]){
         MPI_Recv(vector, sizeVector, MPI_INT, 0, 10, MPI_COMM_WORLD,&status);
         ini=aux*(sizeVector/aux1)+(aux<resto?aux:resto);
         fin=ini+(sizeVector/aux1)+(aux<resto);
+        printf("Proceso: %d \n",rank);
         for(int i=ini;i<fin;i++){
-            if(i%2==0) suma += vector[i];
-            else    suma -= vector[i];
+            printf("%d + %d  \n",suma,vector[i]);
+            suma +=vector[i];
         }
         MPI_Send (&suma, 100, MPI_INT, 0, 10, MPI_COMM_WORLD);  
     }   
-    MPI_Finalize();
     free(vector);
+    MPI_Finalize();
+
 
 }
